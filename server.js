248f60dadeb603967975bd676749ad37f89b50e8
notsovish29 <notsovish29@gmail.com>
2025-11-07 00:35:47 +0530

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static(__dirname));
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
